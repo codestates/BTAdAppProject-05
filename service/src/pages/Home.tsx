9510eebab382c8ebf1aefd40820609c7207ca7c3
choekko <choekko@gmail.com>
2022-10-29 20:54:08 +0900

    [#16] FEAT: 배팅 애니메이션 구현

--- service/src/pages/Home.tsx
+++ service/src/pages/Home.tsx
@@ -3,13 +3,34 @@ import VTable from '@/components/board/vacs/VTable';
 import { css } from '@emotion/react';
 import VChipBundle from '@/components/board/vacs/VChipBundle';
 import VChips from '@/components/board/vacs/VChips';
+import { useState } from 'react';
 
 function Home() {
+  const [bettingCount, setBettingCount] = useState(2);
+
+  const handleChipBundleClick = () => {
+    if (bettingCount > 10) {
+      alert('게임 한 판당 베팅 금액은 100을 초과할 수 없습니다.');
+      return;
+    }
+    setBettingCount(prev => prev + 1);
+  };
+
+  const handleChipsForBettingClick = () => {
+    if (bettingCount === 0) return;
+    setBettingCount(prev => prev - 1);
+  };
+
   return (
     <div css={homeWrapCss}>
       <main css={tableAreaCss}>
-        <VChipBundle cssProps={chipBundleModifierCss} />
-        <VChips cssProps={chipModifierCss} count={3} />
+        <VChipBundle cssProps={chipBundleModifierCss} onClick={handleChipBundleClick} />
+        <VChips
+          cssProps={chipsModifierCss}
+          chipCssProps={chipModifierCss}
+          count={bettingCount}
+          onClick={handleChipsForBettingClick}
+        />
         <div css={tableWrapCss}>
           <VTable />
         </div>
@@ -18,10 +39,41 @@ function Home() {
   );
 }
 
-const chipModifierCss = css`
+const chipsModifierCss = css`
   position: absolute;
   transform: translate(-70px);
   z-index: 2;
+`;
+
+const chipModifierCss = css`
+  animation: myship 1s;
+  -moz-animation: myship 1s; /* Firefox */
+  -webkit-animation: myship 1s; /* Safari and Chrome */
+
+  @keyframes myship {
+    from {
+      transform: translateX(-500px);
+    }
+    to {
+      transform: traslateX(0px);
+    }
+  }
+  @-moz-keyframes myship {
+    from {
+      transform: translateX(-500px);
+    }
+    to {
+      transform: traslateX(0px);
+    }
+  }
+  @-webkit-keyframes myship {
+    from {
+      transform: translateX(-500px);
+    }
+    to {
+      transform: traslateX(0px);
+    }
+  }
 `;
 
 const chipBundleModifierCss = css`
