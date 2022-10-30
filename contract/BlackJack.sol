f53cc1f2431bb8b64cf791860d4603753d0fb4d1
HyeseongMo <hyeseong0914@naver.com>
2022-10-31 00:30:42 +0900

    [no issue] FIX: 상태변수 삭제해서 수수료 줄이는 작업

--- contract/BlackJack.sol
+++ contract/BlackJack.sol
@@ -5,22 +5,23 @@ pragma solidity >=0.7.0 <0.9.0;
 import "contracts/BlackJack_bet.sol";
 
 contract BlackJack is BlackJackBet {
-    uint deckSize = 52;
-    uint[] deck;
-    uint[] dealerCards;
-    uint[] playerCards;
     event completeShuffle(string result, string message);
     event completeAll(string result, string message);
     mapping(address => uint[]) public playerCardDeckMap;
     mapping(address => uint[]) public dealerCardDeckMap;
 
-    function createDeck() public {
+    function createDeck() public pure returns (uint[] memory){
+        uint deckSize = 52;
+        uint[] memory deck = new uint[](52);
         for (uint i = 0; i < deckSize; i++){
-            deck.push(i);
+            deck[i] = i;
         }
+        return deck;
     }
 
-    function shuffleDeck() public {
+    function shuffleDeck(uint[] memory deck) public {
+        uint[] memory dealerCards = new uint[](8);
+        uint[] memory playerCards = new uint[](8);
         for (uint i = 0; i < deck.length; i++) {
             uint n = i + uint(keccak256(abi.encodePacked(block.timestamp))) % (deck.length - i);
             uint temp = deck[n];
@@ -29,32 +30,26 @@ contract BlackJack is BlackJackBet {
         }
 
         for (uint i = 0; i < 6; i++) {
-            uint card = deck[deck.length - 1];
-            deck.pop();
-            playerCards.push(card);
+            uint card = deck[i];
+            playerCards[i] = card;
         }
         playerCardDeckMap[msg.sender] = playerCards;
 
         for (uint i = 6; i < 12; i++) {
-            uint card = deck[deck.length - 1];
-            deck.pop();
-            dealerCards.push(card);
+            uint card = deck[i];
+            dealerCards[i - 6] = card;
         }
         dealerCardDeckMap[msg.sender] = dealerCards;
 
         emit completeShuffle("success", "Card is Ready!");
     }   
 
-    function getDealerCards() public view returns (uint[] memory){
-        return dealerCardDeckMap[msg.sender];
+    function getDealerCards(address user) public view returns (uint[] memory){
+        return dealerCardDeckMap[user];
     }
 
-    function getPlayerCards() public view returns (uint[] memory){
-        return playerCardDeckMap[msg.sender];
-    }
-
-    function getDeck() public view returns (uint[] memory){
-        return deck;
+    function getPlayerCards(address user) public view returns (uint[] memory){
+        return playerCardDeckMap[user];
     }
 
     function playBlackJack() public payable {
@@ -62,9 +57,8 @@ contract BlackJack is BlackJackBet {
         address userAddress = msg.sender;
         userBet(betAmount);
         playerBetAmount[userAddress] = betAmount;
-        createDeck();
-        shuffleDeck();
+        uint[] memory deck = createDeck();
+        shuffleDeck(deck);
         emit completeAll("success", "Play is Ready!");
     }
-
 }
